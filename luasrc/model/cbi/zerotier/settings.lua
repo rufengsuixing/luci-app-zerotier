7d41a4fe66b5418941b0dda2f4d71ecb50457953
Hilman Maulana <hilman0.0maulana@gmail.com>
2023-07-24 20:52:03 +0700

    Update settings.lua

--- luasrc/model/cbi/zerotier/settings.lua
+++ luasrc/model/cbi/zerotier/settings.lua
@@ -1,34 +1,31 @@
+local o = require "luci.sys"
+local a, t, e
+a = Map("zerotier", translate("ZeroTier")
+a.description = translate("Zerotier is an open source, cross-platform and easy to use virtual LAN."))
+a:template = "zerotier/index"
 
-a=Map("zerotier",translate("ZeroTier"),translate("Zerotier is an open source, cross-platform and easy to use virtual LAN"))
-a:section(SimpleSection).template  = "zerotier/zerotier_status"
-
-t=a:section(NamedSection,"sample_config","zerotier")
-t.anonymous=true
-t.addremove=false
-
-e=t:option(Flag,"enabled",translate("Enable"))
-e.default=0
-e.rmempty=false
-
-e=t:option(DynamicList,"join",translate('ZeroTier Network ID'))
-e.password=true
-e.rmempty=false
-
-e=t:option(Flag,"nat",translate("Auto NAT Clients"))
-e.default=0
-e.rmempty=false
-
-e = t:option(MultiValue, "access", translate("Zerotier access control"))
-e.default="lanfwzt ztfwwan ztfwlan"
-e.rmempty=false
-e:value("lanfwzt",translate("lan access zerotier"))
-e:value("wanfwzt",translate("wan access zerotier"))
-e:value("ztfwwan",translate("remote access wan"))
-e:value("ztfwlan",translate("remote access lan"))
-e.widget = "checkbox"
-
-e=t:option(DummyValue,"opennewwindow" , 
-	translate("<input type=\"button\" class=\"cbi-button cbi-button-apply\" value=\"Zerotier.com\" onclick=\"window.open('https://my.zerotier.com/network')\" />"))
+t = a:section(NamedSection, "sample_config", "zerotier")
+t.anonymous = true
+t.addremove = false
+---- status
+e = t:option(DummyValue, "status", translate("Status"))
+e.template = "zerotier/status"
+e.value = translate("Collectiong data...")
+---- enable
+e = t:option(Flag, "enabled", translate("Enable"))
+e.default = 0
+e.rmempty = false
+---- network id
+e = t:option(DynamicList, "join", translate('ZeroTier Network ID'))
+e.password = true
+e.rmempty = false
+---- auto nat
+e = t:option(Flag, "nat", translate("Auto NAT Clients"))
+e.description = translate("Allow zerotier clients access your LAN network")
+e.default = 0
+e.rmempty = false
+---- my zerotier
+e = t:option(DummyValue,"opennewwindow" , translate("<input type=\"button\" class=\"cbi-button cbi-button-apply\" value=\"My ZeroTier\" onclick=\"window.open('https://my.zerotier.com/network')\" />"))
 e.description = translate("Create or manage your zerotier network, and auth clients who could access")
 
 return a
